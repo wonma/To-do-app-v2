b710ca5df7034847abe0ff974f1e5cfb51d5e5ab
Wonmi <wonmadesign@gmail.com>
2018-11-06 21:27:38 +0900

    DOM manipulation practice for todo app

--- script.js
+++ script.js
@@ -1,8 +1,46 @@
-const h1 = document.querySelector('h1')
-const ps = document.querySelectorAll('p')
+const todos = [{
+    text: 'walk a dog',
+    completed: false
+}, {
+    text: 'learn JS',
+    completed: true
+}, {
+    text: 'buy food',
+    completed: false
+}, {
+    text: 'cook dinner',
+    completed: true
+}, {
+    text: 'Be grateful',
+    completed: true
+}]
 
-ps.forEach(function (p) {
-    if(p.textContent.includes('the')) {
-        p.remove()
-    }
+
+const body = document.querySelector('body') // node로 실행하면 당연히 안잡힘.
+const button = document.querySelector('button')
+
+const todoNotCompleted = todos.filter(function(todo) {
+    return todo.completed === false
+})
+const summary = document.createElement('h2')
+summary.textContent = `You have ${todoNotCompleted.length} items to complete`
+body.append(summary)
+
+// The code below only shows the incomplete items.
+//
+// for (let i = 0; i < todoNotCompleted.length; i++) {
+//     const newParagraph = document.createElement('p')
+//     newParagraph.textContent = `${todoNotCompleted[i].text}`
+//     body.append(newParagraph)
+// }
+
+
+todos.forEach(function (todo) {
+    const newItem = document.createElement('p')   // 샘은 const p 라고 이름지음
+    newItem.textContent = todo.text
+    body.append(newItem)
+})
+
+button.addEventListener('click', function (e) {
+    e.target.textContent = 'hahaha'
 })
