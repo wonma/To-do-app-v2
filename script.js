dfec7acd232f784584682e97ee7100711f9d0997
Wonmi <wonmadesign@gmail.com>
2018-11-06 17:05:46 +0900

    Basic set up

---
+++ script.js
@@ -0,0 +1,8 @@
+const h1 = document.querySelector('h1')
+const ps = document.querySelectorAll('p')
+
+ps.forEach(function (p) {
+    if(p.textContent.includes('the')) {
+        p.remove()
+    }
+})
